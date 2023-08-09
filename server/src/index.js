740b0ee5765746bc6e770ebd82db9daf624f21b5
babucarr32 <baboucarry@gmai.com>
2023-08-09 19:02:01 +0000

    Conneced mongoose to mongodb

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+const mongoose = require("mongoose");
+
+const MONGODB_URI =
+  "mongodb+srv://bbabucarr32:ye0QDgQQm8bXk7KL@todoappcluster.rbh3qeu.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.error(err));
+
+// async function startApolloServer() {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+//   const { url } = await startStandaloneServer(server);
+//   console.log(`
+//       🚀  Server is running!
+//       📭  Query at ${url}
+//     `);
+// }
+
+// startApolloServer();
