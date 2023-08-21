00b2bdc0dcdb3b66b3f06a9fcfbd359f976c3d5b
babucarr32 <baboucarry@gmai.com>
2023-08-21 12:58:30 +0000

    Implemented prompt modal before user clears todo

--- client/src/components/ComboBox.tsx
+++ client/src/components/ComboBox.tsx
@@ -20,6 +20,7 @@ import { useAtom } from "jotai";
 import { jotaiSecTodo, jotaiTodo } from "../atoms/JotaiAtoms";
 import useDeleteTodo from "../hooks/useDeleteTodo";
 import { frameworks } from "../variables/vars";
+import Modal from "./Modal";
 
 export function ComboboxDemo() {
   const [open, setOpen] = React.useState(false);
@@ -27,6 +28,7 @@ export function ComboboxDemo() {
   const [, setTodos] = useAtom(jotaiTodo);
   const [secTodos, setSecTodos] = useAtom(jotaiSecTodo);
   const [deleteTodo] = useDeleteTodo();
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
 
   const setActiveTodos = () => {
     const active = secTodos.filter((todo) => todo.completed == false);
@@ -59,55 +61,64 @@ export function ComboboxDemo() {
       setCompletedTodos();
     }
     if (value == "clear completed") {
-      setClearTodos();
+      setIsModalOpen(true);
     }
   };
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] bg-slate-950 flex justify-between items-center p-3 text-white border-2 border-slate-950 rounded-lg"
-        >
-          {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : "Filter todo..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] bg-slate-900 shadow-xl flex flex-col items-center p-3 border-2 border-slate-950 rounded-lg">
-        <Command>
-          <CommandInput
-            placeholder="No framework..."
-            className="p-2 bg-transparent text-white outline-none"
-          />
-          <CommandEmpty className="text-white">No match found...</CommandEmpty>
-          <CommandGroup>
-            {frameworks.map((framework) => (
-              <CommandItem
-                className="p-3 text-white"
-                key={framework.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue);
-                  setOpen(false);
-                  handleFilterTodo(currentValue);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {framework.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        cb={() => setClearTodos()}
+      />
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] bg-slate-950 flex justify-between items-center p-3 text-white border-2 border-slate-950 rounded-lg"
+          >
+            {value
+              ? frameworks.find((framework) => framework.value === value)?.label
+              : "Filter todo..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] bg-slate-900 shadow-xl flex flex-col items-center p-3 border-2 border-slate-950 rounded-lg">
+          <Command>
+            <CommandInput
+              placeholder="No framework..."
+              className="p-2 bg-transparent text-white outline-none"
+            />
+            <CommandEmpty className="text-white">
+              No match found...
+            </CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  className="p-3 text-white"
+                  key={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                    handleFilterTodo(currentValue);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
